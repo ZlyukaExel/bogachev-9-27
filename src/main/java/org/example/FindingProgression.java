69808865cec8295d370d5c9bad85669e2ef4cb2e
ZlyukaExel <126321708+ZlyukaExel@users.noreply.github.com>
2024-12-15 15:39:05 +0300

    jjjdkhdldhdh

--- src/main/java/org/example/FindingProgression.java
+++ src/main/java/org/example/FindingProgression.java
@@ -1,65 +1,86 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
 public class FindingProgression {
     /*public static void main(String[] Args){
         process(Arrays.asList(3, 8, 5, 4, 9, 6, 7, 1, 2));
         process(Arrays.asList(36338, 8, 5, 10, 9, 6, 77247835, 1, 2));
+        process(Arrays.asList(8, 8));
     }*/
     public static List<Integer> integerList;
 
-    public static void process(List<Integer> list){
-        List<Integer> sortedList = new ArrayList<>(list);
-        sort(sortedList);
-        int frst = findMin(sortedList);
-        int scnd = indexOf(sortedList, 1);
-        int d = scnd - frst;
-        int length = sortedList.size();
-        boolean isProgression = true;
+    public static void process(List<Integer> list) {
+        System.out.print(list + "   ->   ");
+        integerList = list;
 
-        for (int i = 0; i < length - 1; i++) {
-            int valOfI = indexOf(sortedList, i);
-            int valOfNextI = indexOf(sortedList, i + 1);
-            if (valOfI + d != valOfNextI)
-            {
-                isProgression = false;
-                break;
-            }
-        }
-
-        if(isProgression)
-            integerList = sortedList;
-        else
-            integerList = list;
-
-        print(list);
-        System.out.print("   ->   ");
-        print(integerList);
-        System.out.println();
+        if (isProgression(list))
+            sort(list);
+        System.out.println(integerList);
     }
 
-    public static int findMax(List<Integer> list){
-        return list.getLast();
+    public static int findMax(List<Integer> list) {
+        int max = Integer.MIN_VALUE;
+        for (Integer integer : list) {
+            if (integer > max) {
+                max = integer;
+            }
+        }
+        return max;
     }
 
     public static int findMin(List<Integer> list) {
-        return list.getFirst();
-    }
-
-    public static int indexOf(List<Integer> list, int value){
-        return list.get(value);
-    }
-
-    public static void sort(List<Integer> list){
-        Collections.sort(list);
-    }
-
-    public static void print(List<Integer> list){
-        for(Integer v : list){
-            System.out.print(v + " ");
+        int min = Integer.MAX_VALUE;
+        for (Integer integer : list) {
+            if (integer < min) {
+                min = integer;
+            }
         }
+        return min;
+    }
+
+    public static int indexOf(List<Integer> list, int value) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void sort(List<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    swap(list, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void swap(List<Integer> list, int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+
+    public static boolean isProgression(List<Integer> list) {
+        int min = findMin(list);
+        int max = findMax(list);
+        int n = list.size();
+
+        if ((max - min) % (n - 1) != 0) return false;
+
+        int d = (max - min) / (n - 1);
+
+        for (int i = 0; i < n; i++) {
+            int expectedValue = min + i * d;
+            if (indexOf(list, expectedValue) == -1) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,9 @@ import java.util.MissingFormatArgumentException;
 
 public class Main {
     public static void main(String[] args) {
-        if (args[0].equals("-a")) {
+        if (args.length > 0 && args[0].equals("-a")) {
             if (args[1].equals("console"))
-                new ConsoleApp(Arrays.copyOfRange(args,2,args.length));
+                new ConsoleApp(Arrays.copyOfRange(args, 2, args.length));
             else if (args[1].equals("window"))
                 new App();
             else
